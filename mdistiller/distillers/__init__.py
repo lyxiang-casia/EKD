1b65073126c3babe554ad57e78096c436fb6183a
lyxiang-casia <xiangliangyu2023@ia.ac.cn>
2025-12-01 15:27:20 +0000

    update

--- mdistiller/distillers/__init__.py
+++ mdistiller/distillers/__init__.py
@@ -15,17 +15,8 @@ from .VID import VID
 from .ReviewKD import ReviewKD
 from .DKD import DKD
 from .Evidential_Teacher import Teacher
-from .Teacher_lamb import Teacher_lamb
 from .EKD import EKD
-from .KD4eModel import KD4eModel
-from .DKD4eModel import DKD4eModel
-from .MLKD4eModel import MLKD4eModel
-from .Monitor import Monitor
-from .EKDIN import EKDIN
 from .CAT_KD import CAT_KD
-from .EKD_WKD_F import EKDWKD
-from .EKD_SimKD import EKDSimKD
-from .EKD_feat_logit import EKD_feat_logit
 
 distiller_dict = {
     "NONE": Vanilla,
